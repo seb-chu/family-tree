386515e928bbf1f552997673329f236f1434cf6f
seb-chu <sebastianclc123@gmail.com>
2024-01-28 22:36:47 -0600

    added previous files for practice

---
+++ src/main/java/com/example/neo4jdatabasepractice/PersonController.java
@@ -0,0 +1,18 @@
+// package com.example.neo4jdatabasepractice;
+
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RestController;
+
+// https://www.baeldung.com/spring-controllers
+// @RestController
+// public class PersonController {
+
+//     @GetMapping(value = "/annotated/person/{firstName}")
+//     public Person getChildRelationships(@PathVariable String firstName) {
+//         Person person = new Person();
+//         person.setFirstName("joe");
+//         person.setChildren("bob");
+//     }
+    
+// }
